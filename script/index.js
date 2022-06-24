2ebad6d63690f3d5130939588b8ff9a36dbcaa6d
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-06-23 22:18:42 -0500

    countdownd date

--- script/index.js
+++ script/index.js
@@ -1,14 +1,18 @@
+import countdown from "./cuenta_regresiva.js";
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
-import { shortcuts } from "./teclados.js";
+import { moveBall, shortcuts } from "./teclados.js";
 
 const d = document;
+
 d.addEventListener("DOMContentLoaded", () => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Oct 1 2022 00:00 ", "Feliz Nada");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
+  moveBall(e, ".ball", ".stage");
 });
--- script/teclados.js
+++ script/teclados.js
@@ -1,4 +1,45 @@
 const d = document;
+let x = 0,
+  y = 0;
+
+export function moveBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBouncingClientRect(),
+    limitsStage = $stage.getBouncingClientRect();
+
+  switch (e.keyCode) {
+    case 37:
+      if (limitsBall.left > limitsStage.left) {
+        x--;
+        e.preventDefault();
+      }
+      break;
+    case 38:
+      if (limitsBall.top > limitsStage.top) {
+        y--;
+        e.preventDefault();
+      }
+      break;
+    case 39:
+      if (limitsBall.right < limitsStage.right) {
+        y++;
+        e.preventDefault();
+      }
+      break;
+    case 40:
+      if (limitsBall.bottom > limitsStage.bottom) {
+        x++;
+        e.preventDefault();
+      }
+      break;
+
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x}px, ${y * 10}px)`;
+}
+
 export function shortcuts(e) {
   if (e.altKey) {
     if (e.key === "a") alert("activado alerta");
